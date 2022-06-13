787aecf9f75b20862eab216731993e36294075b5
tsyFcpn <95341547+tsyFcpn@users.noreply.github.com>
2022-06-13 14:36:23 -0400

    Add files via upload

---
+++ 1/FibonacciPython.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def fibo(n):
+   if n <= 1:
+       return n
+   else:
+       return(fibo(n-1) + fibo(n-2))
+        
+for i in range(5):
+       print(fibo(i))
