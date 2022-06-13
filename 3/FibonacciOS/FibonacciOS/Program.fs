41d0ce5894e69c630a6af0acfefc63b0e6635a1f
tsyFcpn <95341547+tsyFcpn@users.noreply.github.com>
2022-06-13 15:37:12 -0400

    Add files via upload

---
+++ 3/FibonacciOS/FibonacciOS/Program.fs
@@ -0,0 +1,32 @@
+﻿// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+
+// Más información acerca de F# en http://fsharp.net
+// Vea el proyecto "Tutorial de F#" para obtener más ayuda.
+open System
+let rec fib1 n = 
+    match n with
+    | 1 -> 
+        1 
+    | 2 -> 
+        1
+    | 0 -> 
+        0
+    | n -> 
+        fib1(n - 1) + fib1(n - 2)
+
+let forLoop(n) =
+    for i in 1 .. n do
+        printf"%i" (fib1(i))
+        printfn ""
+
+let fibOS f n = 
+    f(n)
+
+[<EntryPoint>]
+let main argv = 
+    let msg = Console.WriteLine("Ingrese n: ")
+    let n = Console.ReadLine()|> int
+    fibOS forLoop n
+    let temp = Console.ReadKey()
+    0 // devolver un código de salida entero
