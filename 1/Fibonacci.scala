07a0abb22630cd0fb82740394ee4dd8f8c85c191
tsyFcpn <95341547+tsyFcpn@users.noreply.github.com>
2022-06-15 09:50:24 -0400

    Add files via upload

---
+++ 1/Fibonacci.scala
@@ -0,0 +1,20 @@
+object Fibo {
+    def recursiveFibo(n:Int):Int = {
+        if (n == 0) {
+            0
+        } else if (n == 1) {
+            1
+        } else if (n == 2) {
+            1
+        } else {
+            (recursiveFibo(n - 1) + recursiveFibo(n - 2))
+        } 
+    }
+    def main(args: Array[String]):Unit = {
+        println("Ingrese n: ")
+        val temp = scala.io.StdIn.readInt()
+        for( i <- 0 to (temp - 1)){
+            recursiveFunc(i)
+        }
+    }   
+}
